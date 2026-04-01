5930c5103d2306ee051fb0054056442e7e5b08e0
Charan6924 <cxv166@case.edu>
2026-04-01 18:02:48 -0400

    finished chapter 6

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
+#include <string>
+
+constexpr bool isEven(int x) {
+    return (x%2 == 0);
+}
 
 int main()
 {
-    std::cout << "Enter the height of the tower : ";
-    int height{};
-    std::cin >> height;
-
-    std::cout << "At 0 seconds, the height of the ball is : " << height << " metres.\n";
-
-    for (int x{1}; x <= 5; x++)
-    {
-        double distance_fallen{ 9.8 * x * x / 2 };
-        double current_height{ static_cast<double>(height) - distance_fallen };
-        if (current_height > 0.0)
-            std::cout << "At " << x << " seconds, the height of the ball is : " << current_height << " metres.\n";
-        else
-            std::cout << "The ball is at the ground.";
+    std::cout << "Enter a number : ";
+    int a{};
+    std::cin >> a;
+    if (isEven(a)) {
+        std::cout << "The number is even";
+        return 0;
     }
-
+    std::cout << "The number is odd";
     return 0;
+
 }
