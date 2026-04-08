0e9d84280b1e2563ba9fd30d123cf7199f7287b9
Charan6924 <cxv166@case.edu>
2026-04-08 16:46:11 -0400

    Finished chapter 8

--- main.cpp
+++ main.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 
-int accumulate(int a);
+double getTowerHeight()
+{
+    std::cout << "Enter the height of the tower in meters: ";
+    double towerHeight{};
+    std::cin >> towerHeight;
+    return towerHeight;
+}
+
+double calculateBallHeight(double towerHeight, int seconds)
+{
+    const double gravity { 9.8 };
+
+    const double fallDistance { gravity * (seconds * seconds) / 2.0 };
+    const double ballHeight { towerHeight - fallDistance };
+
+    if (ballHeight < 0.0)
+        return 0.0;
+
+    return ballHeight;
+}
+
+void printBallHeight(double ballHeight, int seconds)
+{
+    if (ballHeight > 0.0)
+        std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << " meters\n";
+    else
+        std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+}
+
+void calculateAndPrintBallHeight(double towerHeight, int seconds)
+{
+    const double ballHeight{ calculateBallHeight(towerHeight, seconds) };
+    printBallHeight(ballHeight, seconds);
+}
 
 int main()
 {
-    std::cout << accumulate(4) << '\n'; // prints 4
-    std::cout << accumulate(3) << '\n'; // prints 7
-    std::cout << accumulate(2) << '\n'; // prints 9
-    std::cout << accumulate(1) << '\n'; // prints 10
+    const double towerHeight{ getTowerHeight() };
+
+    int num_seconds{};
+    std::cout << "Enter number of seconds : " << "\n";
+    std::cin >> num_seconds;
+    for (int i{0}; i<=num_seconds;i++) {
+        calculateAndPrintBallHeight(towerHeight,i);
+    }
 
     return 0;
 }
