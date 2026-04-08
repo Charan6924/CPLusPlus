ed4fcface0e4be78f3c3c748048232f165f0f0df
Charan6924 <cxv166@case.edu>
2026-04-08 17:55:30 -0400

    Finished chapter 9 and 10

--- main.cpp
+++ main.cpp
@@ -1,50 +1,19 @@
 #include <iostream>
 
-double getTowerHeight()
-{
-    std::cout << "Enter the height of the tower in meters: ";
-    double towerHeight{};
-    std::cin >> towerHeight;
-    return towerHeight;
+namespace constants {
+    constexpr double pi {3.14159};
+}
+using Degrees = double;
+using Radians = double;
+double convertToRadians(Degrees degrees) {
+    return degrees * constants::pi / 180;
 }
 
-double calculateBallHeight(double towerHeight, int seconds)
-{
-    const double gravity { 9.8 };
-
-    const double fallDistance { gravity * (seconds * seconds) / 2.0 };
-    const double ballHeight { towerHeight - fallDistance };
-
-    if (ballHeight < 0.0)
-        return 0.0;
-
-    return ballHeight;
-}
-
-void printBallHeight(double ballHeight, int seconds)
-{
-    if (ballHeight > 0.0)
-        std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << " meters\n";
-    else
-        std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
-}
-
-void calculateAndPrintBallHeight(double towerHeight, int seconds)
-{
-    const double ballHeight{ calculateBallHeight(towerHeight, seconds) };
-    printBallHeight(ballHeight, seconds);
-}
-
-int main()
-{
-    const double towerHeight{ getTowerHeight() };
-
-    int num_seconds{};
-    std::cout << "Enter number of seconds : " << "\n";
-    std::cin >> num_seconds;
-    for (int i{0}; i<=num_seconds;i++) {
-        calculateAndPrintBallHeight(towerHeight,i);
-    }
-
+int main() {
+    std::cout << "Enter a number in degrees : ";
+    Degrees degrees{};
+    std::cin >> degrees;
+    Radians radians {convertToRadians(degrees)};
+    std::cout << degrees << " degrees is " << radians << " radians";
     return 0;
 }
