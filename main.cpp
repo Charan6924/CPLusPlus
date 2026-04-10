f5915da140977c18e0f0e33569a7df1e41fece7e
Charan6924 <cxv166@case.edu>
2026-04-09 21:33:15 -0400

    done chapter 12

--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
+#include <algorithm>
+
+void sort2(int& x, int& y) {
+    std::swap(x,y);
+}
 
 int main()
 {
-   int x{ 5 };
-   int& ref { x };
-   int* ptr { &x };
+    int x { 7 };
+    int y { 5 };
 
-   std::cout << x;
-   std::cout << ref;
-   std::cout << *ptr << '\n';
+    std::cout << x << ' ' << y << '\n'; // should print 7 5
 
-   ref = 6;
-   std::cout << x;
-   std::cout << ref;
-   std::cout << *ptr << '\n';
+    sort2(x, y);
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
 
-   *ptr = 7;
-   std::cout << x;
-   std::cout << ref;
-   std::cout << *ptr << '\n';
+    sort2(x, y);
+    std::cout << x << ' ' << y << '\n'; // should print 5 7
 
-   return 0;
+    return 0;
 }
